0f5ee15069a78189a0a154fd54bbda5ab4ffcd1c
bimamib <bimaprast1601@gmail.com>
2023-09-13 00:13:23 +0700

    memanggil fungsi MainViewModel

--- app/src/main/java/com/bima/mylivedata/MainActivity.kt
+++ app/src/main/java/com/bima/mylivedata/MainActivity.kt
@@ -2,10 +2,19 @@ package com.bima.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.bima.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 }
